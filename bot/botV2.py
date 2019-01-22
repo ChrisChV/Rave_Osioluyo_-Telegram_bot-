cb4a84de904bbfd56a985068011629809a228d8d
ChrisChV <cristo2014f@outlook.es>
2019-01-22 18:09:33 -0500

    APP deply

--- bot/botV2.py
+++ bot/botV2.py
@@ -16,19 +16,20 @@ def caps(bot, update, args):
     text_caps = ' '.join(args).upper()
     bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
-updater = Updater(token='717635382:AAE9Qy-9Vd0wAsUAVnII9y9CLE-8E-s9EAA')
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+def initServer():
+    updater = Updater(token='717635382:AAE9Qy-9Vd0wAsUAVnII9y9CLE-8E-s9EAA')
+    dispatcher = updater.dispatcher
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-caps_handler = CommandHandler('caps', caps, pass_args=True)
-dispatcher.add_handler(caps_handler)
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    echo_handler = MessageHandler(Filters.text, echo)
+    dispatcher.add_handler(echo_handler)
+    caps_handler = CommandHandler('caps', caps, pass_args=True)
+    dispatcher.add_handler(caps_handler)
 
 
-updater.start_polling()
+    updater.start_polling()
 
-updater.idle()
+    updater.idle()
---
+++ page/server.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.insert(0, '../bot/')
+
+from botV2 import *
+
+initServer()
+
