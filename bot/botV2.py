c8378fa3618340ad81780a887962536bdd7794d7
ChrisChV <cristo2014f@outlook.es>
2019-01-22 18:42:53 -0500

    changed all

--- bot/botV2.py
+++ bot/botV2.py
@@ -5,6 +5,7 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import os
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
@@ -17,7 +18,13 @@ def caps(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
 def initServer():
-    updater = Updater(token='717635382:AAE9Qy-9Vd0wAsUAVnII9y9CLE-8E-s9EAA')
+
+    TOKEN = '717635382:AAE9Qy-9Vd0wAsUAVnII9y9CLE-8E-s9EAA'
+    NAME = 'rave-osioluyo'
+
+    PORT = os.environ.get('PORT')
+
+    updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
@@ -32,4 +39,8 @@ def initServer():
 
     updater.start_polling()
 
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
     updater.idle()
