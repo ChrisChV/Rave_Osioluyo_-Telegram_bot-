c2fdd1ef9feaf8994a5a1bbaf42cd0ff402c883b
Christofer Chavez <xnpiochv@gmail.com>
2019-02-25 12:44:48 -0500

    finish the basic program

---
+++ rave/src/sendMsg.py
@@ -0,0 +1,21 @@
+import sad
+import os
+import json
+import requests
+
+
+def sendMsg(message):
+    tokenFile = open(sad.TOKEN_FILE, 'r')
+    token = tokenFile.readline()
+    tokenFile.close()
+    jsonStructureFile = open(sad.JSON_STRUCTURE_FILE_NAME, 'r')
+    data = json.load(jsonStructureFile)
+
+    data["message"]["text"] = "/_sendMsg " + token + " " + message
+    #data["message"]["id"] = 123456
+    #data["message"]["chat"]["id"] = 123456
+    jsonData = json.dumps(data)
+    res = requests.post(sad.WEB_HOOK_URL, json = data)
+
+
+sendMsg("Hola")
