19411962596c2414e9f60ed6e66541c8d29aa265
Christofer Chavez <xnpiochv@gmail.com>
2019-02-25 14:26:41 -0500

    finish basic program

---
+++ rave/src/main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import sad
+from sendMsg import *
+
+
+
+command = "hostname > " + sad.TEMP_HOST_FILE
+os.system(command)
+tempFile = open(sad.TEMP_HOST_FILE, 'r')
+host = tempFile.readline().rstrip('\n')
+tempFile.close()
+command = "rm -f " + sad.TEMP_HOST_FILE
+os.system(command)
+
+initMsg = host + " turn on"
+sendMsg(initMsg)
